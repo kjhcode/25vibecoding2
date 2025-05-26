8bc7c59c243ab81b40648ff1cf7c4982ae7631f7
joung hee <84551246+kjhcode@users.noreply.github.com>
2025-05-26 19:35:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ st.write("아래에 장소 정보를 입력하고 지도에 표시해보세요!"
 
 # 장소 입력
 place = st.text_input("장소 이름", value="광주동신여자고등학교")
-lat = st.number_input("위도 (Latitude)", value=37.5665, format="%.6f")
-lon = st.number_input("경도 (Longitude)", value=126.9780, format="%.6f")
+lat = st.number_input("위도 (Latitude)", value=35.161361, format="%.6f")
+lon = st.number_input("경도 (Longitude)", value=126.881795, format="%.6f")
 
 # 세션 상태 저장
 if "places" not in st.session_state:
